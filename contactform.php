b8b0fca73589720bb30f5d7e845c77172d84ca58
Ice4ever <34755789+Ice4ever@users.noreply.github.com>
2020-05-05 12:02:37 +0200

    css/index change/contact form.

---
+++ contactform.php
@@ -0,0 +1,21 @@
+<?php
+
+if(isset($_POST['submit'])){
+        
+        $name = $_POST['name'];
+        $subject = $_POST['subject'];
+        $mailFrom = $_POST['email'];
+        $message = $_POST['message'];
+        
+        $mailTo = "Ice4get@googlemail.com";
+        $headers = "From: ".$mailFrom;
+        $txt = "Du hast eine Email von".$name."bekommen.".$message;
+    
+        mail($mailTo, $subject, $txt, $headers);
+    
+        header("Location: index.html?mailsend")
+    }
+
+
+
+?>
