f7084aaf3e0b2b8166bf3577e41eb68cfb019078
Ice4ever <34755789+Ice4ever@users.noreply.github.com>
2020-05-08 22:03:41 +0200

    big changes

--- assets/js/selfscript.js
+++ assets/js/selfscript.js
@@ -2,7 +2,7 @@ const lightbox = document.createElement('div')
 lightbox.id = 'lightbox'
 document.body.appendChild(lightbox)
 
-const images = document.querySelectorAll('img')
+const images = document.querySelectorAll('.view')
 images.forEach(image => {
     image.addEventListener('click', e => {
         lightbox.classList.add('active')
@@ -21,3 +21,4 @@ lightbox.addEventListener('click', e => {
     lightbox.classList.remove('active')
     
 })
+
